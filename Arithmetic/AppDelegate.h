736068d5c0a20f9016d610ce2c2ecae3e97ec9c8
我 <kesen.@kedemacbook-pro.local>
2018-05-07 10:45:22 +0800

    Initial Commit

---
+++ Arithmetic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Arithmetic
+//
+//  Created by ke sen. on 2018/5/7.
+//  Copyright © 2018年 ke sen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
